dbd97db73d032202bf01cd91040f5fae5c0e8569
Gerardo Hernandez <edgewl2@gmail.com>
2023-11-30 00:54:37 -0600

    fix(main): changes names in messages input and output

--- main.py
+++ main.py
@@ -7,6 +7,6 @@
 import calculator
 
 if __name__ == '__main__':
-    diameter = int(input('Escriba el diametro de la circunferencia: '))
-    result = calculator.print_circum(diameter)
-    print(f'El radio es: {result}')
+    radius = int(input('Escriba el radio de la circunferencia: '))
+    result = calculator.print_circum(radius)
+    print(f'El diametro es: {result}')
