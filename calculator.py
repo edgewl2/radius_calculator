8d76620a2fdadf829eebcd52d5c26ecd55c9fd8f
Gerardo Hernandez <edgewl2@gmail.com>
2023-11-30 00:48:47 -0600

    perf(calculator): improve the function for calculate array parameter

--- calculator.py
+++ calculator.py
@@ -7,3 +7,11 @@ import math
 def print_circum(radius):
     if isinstance(radius, int):
         return round(2 * round(math.pi, 5) * radius, 2)
+    else:
+        radius_count = len(radius)
+        if radius_count > 1:
+            circum = [None] * radius_count
+            for iterator in range(radius_count):
+                value = 2 * round(math.pi, 5) * radius[iterator]
+                circum[iterator] = round(value, 2)
+            return circum
