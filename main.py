75dec081a576eb65574b7279dbb38111ccd950a8
Gerardo Hernandez <edgewl2@gmail.com>
2023-11-30 00:41:18 -0600

    perf(main): add three calling inputs and diameter circumference calculator
    	1. Use iterator for the input values and display the results.
    	2. Condition for a single execution.

--- main.py
+++ main.py
@@ -3,10 +3,19 @@
 
     1. Require import the calculator file.
 """
-
 import calculator
 
 if __name__ == '__main__':
-    radius = int(input('Escriba el radio de la circunferencia: '))
-    result = calculator.print_circum(radius)
-    print(f'El diametro es: {result}')
+    __INPUT_COUNT__ = 3
+    # __INPUT_COUNT__ = int(input('Escriba el numero calculos a hacer: '))
+    if __INPUT_COUNT__ > 1:
+        radius = [None] * __INPUT_COUNT__
+        for iterator in range(__INPUT_COUNT__):
+            radius[iterator] = int(input(f'Escriba el radio de la circunferencia {iterator + 1}: '))
+        result = calculator.print_circum(radius)
+        for item in result:
+            print(f'El diametro {result.index(item) + 1} es: {item}')
+    else:
+        radius = int(input('Escriba el radio de la circunferencia: '))
+        result = calculator.print_circum(radius)
+        print(f'El diametro es: {result}')
